ba633e3fce8a2675d24b4f0e971a53d22425689d
ezerfrlux <aaron.salinas318@gmail.com>
2026-01-03 11:50:51 -0600

    [animation] -  when square down do an animation

--- app/animation-101/index.tsx
+++ app/animation-101/index.tsx
@@ -1,28 +1,36 @@
 import ThemedButton from "@/presentation/shared/ThemedButton";
 import ThemedView from "@/presentation/shared/ThemedView";
 import React, { useRef } from "react";
-import { Animated } from "react-native";
+import { Animated, Easing } from "react-native";
 
 export default function Animation101Screen() {
 
   const animatedOpacity = useRef(new Animated.Value(0)).current;
+  const animatedTop = useRef(new Animated.Value(-100)).current;
 
   const fadeIn = ( ) => {
     Animated.timing(animatedOpacity, {
       toValue: 1,
       duration: 300,
-      useNativeDriver: true
+      useNativeDriver: true,
+    }).start()
+
+    Animated.timing(animatedTop, {
+      toValue:0,
+      duration:700,
+      useNativeDriver: true,
+      easing: Easing.bounce
     }).start()
   }
   const fadeOut = () => {
     Animated.timing(animatedOpacity, {
       toValue: 0,
       duration:300,
-      useNativeDriver: true
-    }).start()
+      useNativeDriver: true,
+    }).start(() => animatedTop.resetAnimation())
+
+
   }
-
-
 
   return (
     <ThemedView margin className="items-center justify-center flex-1">
@@ -31,7 +39,12 @@ export default function Animation101Screen() {
           style={{
             width:150,
             height:150,
-            opacity: animatedOpacity
+            opacity: animatedOpacity,
+            transform:[
+              {
+                translateY:animatedTop
+              }
+            ]
           }}
         />
 
