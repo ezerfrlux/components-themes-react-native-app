b1ba106d9b2a91b78c450d9aff2235ceae817bcf
ezerfrlux <aaron.salinas318@gmail.com>
2026-01-04 11:08:18 -0600

    [ADD] - Implemented alert functionality with two and three button options in AlertScreen

--- app/alerts/index.tsx
+++ app/alerts/index.tsx
@@ -1,10 +1,44 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import ThemedButton from "@/presentation/shared/ThemedButton";
+import ThemedView from "@/presentation/shared/ThemedView";
+import React from "react";
+import { Alert } from "react-native";
 
 export default function AlertScreen() {
+  const createTwoButtonAlert = () => {
+    Alert.alert("Yoo Bro", "Im you in the past 2026 4th january", [
+      {
+        text: "Ask me later",
+        onPress: () => console.log("Ask me later Pressed"),
+        style: "cancel",
+      },
+      {
+        text: "Cancel",
+        onPress: () => console.log("Cancel Pressed"),
+        style: "destructive",
+      },
+      {
+        text: "Ok",
+        onPress: () => console.log("Ok Pressed"),
+      },
+    ]);
+  };
+  const createThreeButtonAlert = () => {
+    Alert.alert("Custom Alert", "Im trying alerts in buttons react native", [
+      {
+        text: "Cancel",
+        onPress: () => console.log("Cancel Pressed"),
+        style: "cancel",
+      },
+      {
+        text: "Ok",
+        onPress: () => console.log("Ok Pressed"),
+      },
+    ]);
+  };
   return (
-    <View>
-      <Text>AlertScreen</Text>
-    </View>
-  )
+    <ThemedView margin className="gap-5">
+      <ThemedButton onPress={createTwoButtonAlert}>2026</ThemedButton>
+      <ThemedButton onPress={createThreeButtonAlert}>Button Alert</ThemedButton>
+    </ThemedView>
+  );
 }
