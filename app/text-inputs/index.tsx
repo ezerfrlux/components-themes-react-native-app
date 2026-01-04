4c91c47cbff3aa5fd4fdfc369db8b234a83b07ed
ezerfrlux <aaron.salinas318@gmail.com>
2026-01-04 11:20:13 -0600

    [ADD] - Implemented TextInputsScreen with ThemedTextInput for user input handling

--- app/text-inputs/index.tsx
+++ app/text-inputs/index.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import ThemedCard from "@/presentation/shared/ThemedCard";
+import ThemedText from "@/presentation/shared/ThemedText";
+import ThemedTextInput from "@/presentation/shared/ThemedTextInput";
+import ThemedView from "@/presentation/shared/ThemedView";
+import React, { useState } from "react";
 
 export default function TextInputsScreen() {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    phone: "",
+  });
   return (
-    <View>
-      <Text>TextInputsScreen</Text>
-    </View>
-  )
+    <ThemedView margin>
+      <ThemedCard className="mb-5">
+        <ThemedTextInput 
+          placeholder="name"
+          autoCapitalize="words"
+          autoCorrect={false}
+          onChangeText={(text) => setForm({ ...form, name: text })} 
+        />
+
+      </ThemedCard>
+      <ThemedCard>
+        <ThemedText>{JSON.stringify(form, null, 2)}</ThemedText>
+      </ThemedCard>
+    </ThemedView>
+  );
 }
