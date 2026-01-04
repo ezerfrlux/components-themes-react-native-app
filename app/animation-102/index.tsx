28f0dfdd031a40e45f5b06a051ab664e43d5d5ab
ezerfrlux <aaron.salinas318@gmail.com>
2026-01-04 09:16:32 -0600

    [UI] - Dragable square with react animated

--- app/animation-102/index.tsx
+++ app/animation-102/index.tsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import { Text, View } from 'react-native'
-
+import ThemedView from "@/presentation/shared/ThemedView";
+import React, { useRef } from "react";
+import { Animated, PanResponder } from "react-native";
+// ? PAN RESPONDER DETECT TACTILE GESTURES
 export default function Animation102Page() {
+  const pan = useRef(new Animated.ValueXY()).current;
+
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: Animated.event([
+      null,
+      {
+        dx: pan.x,
+        dy: pan.y,
+      },
+    ], {useNativeDriver:false}),
+    onPanResponderRelease: () => {
+      Animated.spring(pan, {
+        toValue: { x: 0, y: 0 },
+        useNativeDriver: false,
+      }).start();
+    },
+  });
+
   return (
-    <View>
-      <Text>Animation102Page</Text>
-    </View>
-  )
+    <ThemedView className="items-center justify-center">
+      <Animated.View
+        {...panResponder.panHandlers}
+        className="bg-light-secondary dark:bg-dark-secondary rounded-xl"
+        style={[pan.getLayout(),{ height: 130, width: 130 }]}
+      />
+    </ThemedView>
+  );
 }
